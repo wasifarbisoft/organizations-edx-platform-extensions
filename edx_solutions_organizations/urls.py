746cbb55b0a12372b7f981b7ea2750fc771789c4
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2019-10-28 15:25:01 +0500

    Django version updated to 1.11

--- edx_solutions_organizations/urls.py
+++ edx_solutions_organizations/urls.py
@@ -1,13 +1,12 @@
 """ Organizations API URI specification """
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from edx_solutions_organizations import views as organizations_views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^(?P<organization_id>[0-9]+)/groups/(?P<group_id>[0-9]+)/users$',
         organizations_views.OrganizationsGroupsUsersList.as_view()),
     url(r'^(?P<organization_id>[0-9]+)/attributes',
         organizations_views.OrganizationAttributesView.as_view()),
-)
+]
